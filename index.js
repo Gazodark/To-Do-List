9bee36e188df2266f591a82713af3d81d59b29cd
Gazodark <yvanbryandongmoyemeli@gmail.com>
2025-04-29 22:38:53 +0100

    Add functionality to add new tasks was implemented

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import './style.css';
+import { addTask } from './module/addTask.js';
+
+const todoInput = document.getElementById('todo-input');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+const clearCompletedBtn = document.getElementById('clear-completed-btn');
+
+addBtn.addEventListener('click', () => {
+  const taskText = todoInput.value.trim();
+  if (taskText) {
+    addTask(taskText, todoList);
+    todoInput.value = '';
+  }
+});
---
+++ module/addTask.js
@@ -0,0 +1,9 @@
+export function addTask(taskText, todoList) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox">
+      <span class="task">${taskText}</span>
+      <button class="delete-btn">Delete</button>
+    `;
+    todoList.appendChild(li);
+  }
