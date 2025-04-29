e80193dcb489951b6c2ca8bcbc80986ca696afa3
Gazodark <yvanbryandongmoyemeli@gmail.com>
2025-04-29 01:09:41 +0100

    webpack was installed and a plain html of our to-do list was implemented

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // cleans dist folder before each build
+  },
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
