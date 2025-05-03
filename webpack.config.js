878ebaa4bd36707ece13319be7ba52978f814e1a
Gazodark <yvanbryandongmoyemeli@gmail.com>
2025-05-03 23:42:13 +0100

    my directories were reorganised and the add funtionallity was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     clean: true, // cleans dist folder before each build
   },
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    compress: true,
+    port: 9000,
   },
   mode: 'development',
   module: {
